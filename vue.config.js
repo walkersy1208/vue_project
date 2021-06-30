b1a8de3697d43f3ad0303a4ef9766cf2276af3c0
walker <walkersy1118@gamil.com>
2021-06-30 15:30:23 +0800

    vue_project

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {1111111111
+    devServer: {
+        host: '0.0.0.0',
+        public: '8.135.1.18111114:8080',//该网络地址为你联网的ip地址
+        port: 8080,
+        https: false,
+        hotOnly: false,
+        disableHostCheck: true,
+    },
+}
